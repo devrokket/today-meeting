8fe16967902b41f8c42633e69c94437da8aca7fd
Byeong Rok <rokany@cau.ac.kr>
2022-11-02 00:29:21 +0900

    persisting-data

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -1,8 +1,55 @@
-//
-//  ScrumStore.swift
-//  Scrumdinger
-//
-//  Created by Rocket on 2022/11/01.
-//
-
 import Foundation
+import SwiftUI
+
+//ObservableObject: a class-constrained protocol for connecting external model data to SwiftUI views.
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                        in: .userDomainMask,
+                                        appropriateFor: nil,
+                                        create: false)
+        .appendingPathComponent("scrums.data")
+    }
+    //ScrumStore.view에서는 load함수, store 함수를 만들어 데이터를 저장 및 로드.
+    static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            //do catch statemet --> load 중 발생하는 에러 캐치하기 위함이다.
+            do {
+                let fileURL  = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                                   completion(.success(dailyScrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileURL()
+                try data.write(to: outfile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
+
