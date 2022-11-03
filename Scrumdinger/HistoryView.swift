c6bb25d53d5fef159790137706c8ab4d180a4e40
Byeong Rok <rokany@cau.ac.kr>
2022-11-03 23:49:22 +0900

    History view

---
+++ Scrumdinger/HistoryView.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by Rocket on 2022/11/03.
+//
+
+import Foundation
---
+++ Scrumdinger/Models/SpeechRecognizer.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechRecognizer.swift
+//  Scrumdinger
+//
+//  Created by Rocket on 2022/11/02.
+//
+
+import Foundation
