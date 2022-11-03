12cbbaf31aa32d0266b87d3085490f73ffbf281e
Byeong Rok <rokany@cau.ac.kr>
2022-11-03 23:50:43 +0900

    b

--- Scrumdinger/HistoryView.swift
+++ Scrumdinger/HistoryView.swift
@@ -1,8 +1,17 @@
-//
-//  HistoryView.swift
-//  Scrumdinger
-//
-//  Created by Rocket on 2022/11/03.
-//
-
 import Foundation
+
+struct History: Identifiable, Codable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+    var lengthInMinutes: Int
+    var transcript: String?
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transcript: String? = nil) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.transcript = transcript
+    }
+}
