9295ff498479a1764928a46721c543fd8a8d0a8f
Byeong Rok <rokany@cau.ac.kr>
2022-11-04 00:08:59 +0900

    HistoryView 완성

--- Scrumdinger/HistoryView.swift
+++ Scrumdinger/HistoryView.swift
@@ -1,17 +1,44 @@
-import Foundation
+import SwiftUI
 
-struct History: Identifiable, Codable {
-    let id: UUID
-    let date: Date
-    var attendees: [DailyScrum.Attendee]
-    var lengthInMinutes: Int
-    var transcript: String?
+struct HistoryView: View {
+    let history: History
 
-    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee], lengthInMinutes: Int = 5, transcript: String? = nil) {
-        self.id = id
-        self.date = date
-        self.attendees = attendees
-        self.lengthInMinutes = lengthInMinutes
-        self.transcript = transcript
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding()
+                Text("참석자")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("기록")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
     }
 }
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var history: History {
+        History(attendees: [DailyScrum.Attendee(name: "김효수"), DailyScrum.Attendee(name: "손봉수"), DailyScrum.Attendee(name: "김형태")], lengthInMinutes: 10, transcript: "오늘 회의 주제: 어떻게 열심히 살 수 있을까?에 관하여...")
+    }
+    
+    static var previews: some View {
+        HistoryView(history: history)
+    }
+}
+
+
